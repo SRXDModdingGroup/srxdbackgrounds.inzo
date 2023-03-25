a65e79d0c4d33ec0ab39246d1724abc518b0204d
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-03-24 22:24:34 -0700

    Update Plugin

--- Scripts/Plugin.cs
+++ Scripts/Plugin.cs
@@ -2,6 +2,7 @@ using BepInEx;
 
 namespace SRXDBackgrounds.Inzo {
     [BepInDependency("srxd.customvisuals", "1.2.5")]
+    [BepInDependency("srxd.backgrounds.common","1.0.2")]
     [BepInPlugin("srxd.backgrounds.inzo", "srxdbackgrounds.inzo", "1.0.2")]
     public class Plugin : BaseUnityPlugin {
         private void Awake() => Destroy(gameObject);
